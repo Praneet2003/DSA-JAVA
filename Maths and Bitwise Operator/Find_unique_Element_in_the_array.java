ff4340c4bfe068e982ae32f51f079d2aa009768a
Praneet2003 <rajpraneet4@gmail.com>
2025-07-31 08:57:36 +0530

    Find unique Elements in the array

--- Maths and Bitwise Operator/Find_unique_Element_in_the_array.java
+++ Maths and Bitwise Operator/Find_unique_Element_in_the_array.java
@@ -3,7 +3,7 @@ class Find_unique_Element_in_the_array{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        // int arr[] = {2,3,4,1,2,1,3,6,4};
+        // int arr[] = {2,3,4,1,2,1,3,6,4};  
         int arr[] = new int[n];
         for(int i = 0;i<n;i++){//[2,3,4,1,2,1,3,6,4]
             arr[i] = sc.nextInt();
