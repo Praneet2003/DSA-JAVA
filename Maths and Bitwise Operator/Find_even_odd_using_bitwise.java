59285c7ae2d7cc3794708a16d8f31fe29c9c7a60
Praneet2003 <rajpraneet4@gmail.com>
2025-08-08 08:32:36 +0530

    Determining number is odd or even using (n&1).

--- Maths and Bitwise Operator/Find_even_odd_using_bitwise.java
+++ Maths and Bitwise Operator/Find_even_odd_using_bitwise.java
@@ -6,7 +6,7 @@ class Find_even_odd_using_bitwise{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number to check Even or Odd: ");
         int n = sc.nextInt();
-        if((n & 1) == 1){
+        if((n & 1) == 1){//here n & 1 give the 1st bit which decides odd or even.
             System.out.println("Odd");
         }else{
             System.out.println("Even");
