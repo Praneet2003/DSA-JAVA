ebe4ac41d374e9edb7adecf93fbb60910c71db9f
Praneet2003 <rajpraneet4@gmail.com>
2025-09-14 21:32:06 +0530

    Get the Node Value by first getting the node then returning its value

---
+++ Linked List/Singly Linked List/Get_Node_value.java
@@ -0,0 +1,108 @@
+public class Get_Node_value{
+    private Node head;
+    private Node tail;
+    private int size;
+    public Get_Node_value(){
+        this.size=0;
+    }
+    private class Node{
+        private int value;
+        private Node next;
+        public Node(int data){
+            this.value = data;
+            this.next = null;//initially it is null.
+        }
+        public Node(int data, Node next){
+            this.value = data;
+            this.next=next;
+        }
+    }
+    public void Insert_first(int val){
+        Node newnode = new Node(val);
+        newnode.next = head;
+        head = newnode;
+        if(tail==null){
+            tail=head;
+        }
+        size+=1;
+    }
+    public void Insert_end(int val){
+        Node newnode = new Node(val);
+        if(head==null){
+            head = newnode;
+            tail = newnode;
+        }else{
+            tail.next = newnode;
+            tail = newnode;
+        }
+        size+=1;
+    }
+    public void Insert_In_Middle(int pos,int val){
+        Node newnode = new Node(val);
+        if(pos==1){//handles insertion at first node.
+            newnode.next = head;
+            head = newnode;
+            if(tail==null){
+                tail = head;
+            }
+            size++;
+            return;
+        }
+        if(head==null){//handles insertion in an empty LL
+            if(pos!=1){
+                System.out.println("Invalid position to insert");
+                return;
+            }
+        }
+        Node temp = head;
+        int i=1;
+        while(i<pos-1 && temp!=null){
+            temp = temp.next;
+            i++;
+        }
+        if(temp==null){
+            System.out.println("Invalid position to insert");
+            return;
+        }
+        newnode.next = temp.next;
+        temp.next = newnode;
+        if(newnode.next==null){//i.e newnode is the last node hence we should need to update the tail.
+            tail = newnode;
+        }
+        size++;
+    }
+    public Node get(int index){
+        Node temp = head;
+        for(int i=1;i<index;i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+    public int valueAt(int index){
+        Node ans = get(index);
+        return ans.value;
+    }
+    public void display(){
+        Node temp = head;
+        System.out.println("");
+        while(temp!=null){
+            System.out.print(temp.value+"->");
+            temp = temp.next;
+        }
+        System.out.print("Null");
+    }
+    public static void main(String[] args){
+        Get_Node_value obj = new Get_Node_value();
+        obj.Insert_first(10);
+        obj.Insert_first(20);
+        obj.Insert_first(13);
+        obj.Insert_first(2);
+        obj.Insert_first(23);
+        obj.Insert_end(16);//23->2->13->20->10->16->Null
+        obj.Insert_In_Middle(3, 100);
+        obj.Insert_In_Middle(6, 40);
+        obj.display();
+        System.out.println("\nNo of elements in the Linked List is: "+obj.size);
+        System.out.println("The value at index 2 is: "+obj.valueAt(2));
+    }
+}
