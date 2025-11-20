f46d2b93442b040854b2f8f185050000f22875c3
Praneet2003 <rajpraneet4@gmail.com>
2025-11-20 11:26:55 +0530

    first commit

---
+++ BackTracking/N Knights/N_Knights.java
@@ -0,0 +1,59 @@
+class N_Knights{
+    public static void main(String[] args) {
+        int n = 2;
+        boolean board[][] = new boolean[n][n];
+        knights(board, 0, 0, n);
+    }
+    public static void knights(boolean board[][],int r,int c, int target){
+        if(target==0){
+            display(board);
+            System.out.println("");
+            return;
+        }
+        if(c==board.length){
+            knights(board, r+1, 0, target);
+            return;
+        }
+        if(r>=board.length){
+            return;
+        }
+        if(isSafe(board,r,c)){
+            board[r][c]=true;
+            knights(board, r, c+1, target-1);//if safe then store the ans and move ahead.
+            board[r][c]=false;
+        }
+        knights(board, r, c+1, target);//it it is not safe then also move ahead.
+    }
+    public static boolean isSafe(boolean board[][],int r, int c){
+        if(isValid(board, r-2, c-1) && (board[r-2][c-1])){
+                return false;
+        }
+        if(isValid(board, r-2, c+1) && (board[r-2][c+1])){
+                return false;
+        }if(isValid(board, r-1, c-2) && (board[r-1][c-2])){
+                return false;
+        }
+        if(isValid(board, r-1, c+2) && (board[r-1][c+2])){
+                return false;
+        }
+        return true;
+    }
+    public static boolean isValid(boolean board[][],int r, int c){
+        if(r>=0 && r<board.length && c>=0 && c<board.length){
+            return true;
+        }
+        return false;
+    }
+    public static void display(boolean board[][]){
+        for(boolean row[]:board){
+            for(boolean element:row){
+                if(element){
+                System.out.print("K ");
+                }else{
+                    System.out.print("o ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
