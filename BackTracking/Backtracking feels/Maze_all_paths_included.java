0b698817c200db028853ef85522ff5b664e0e186
Praneet2003 <rajpraneet4@gmail.com>
2025-12-08 15:00:35 +0530

    finding all maze paths showing importantance of backtrancking and counting those paths as well

--- BackTracking/Backtracking feels/Maze_all_paths_included.java
+++ BackTracking/Backtracking feels/Maze_all_paths_included.java
@@ -4,15 +4,19 @@ class Maze_all_paths_included{
                         {true,false,true},
                         {true,true,true}};
         paths("",arr,0,0);
+        System.out.println(count);
     }
+    static int count =0;
     public static void paths(String p, boolean arr[][],int r,int c){
         if(r==arr.length-1 && c==arr[0].length-1){
             System.out.print(p+" ");
+            count++;
             return;
         }
         if(!arr[r][c]){
             return;
         }
+        arr[r][c]=false;
         if(r<arr.length-1){
             paths(p+"D", arr, r+1, c);
         }
@@ -25,6 +29,7 @@ class Maze_all_paths_included{
         if(c>0){
             paths(p+"L", arr, r, c-1);
         }
+        arr[r][c]=true;
     }
 }
-//here the stack is going Overflow. 
+//here the stack is going Overflow. without backtracking
