8d63a173945b61efb8290c895051c6dda580db5f
Praneet2003 <rajpraneet4@gmail.com>
2026-01-28 23:10:35 +0530

    Finding the index of peak element in the mountain array using binary search

---
+++ Searching Algorithms/Binary Search/LeetCode Questions/Peak_index_in_mountain_array.java
@@ -0,0 +1,22 @@
+class Peak_index_in_mountain_array{
+    public static void main(String[] args) {
+        int arr[] ={1, 5,10,6,3,2};
+        System.out.println("Index of the peak element is: "+Binary_search_index(arr));
+    }
+    public static int Binary_search_index(int[] arr){
+        int s = 0;
+        int e = arr.length - 1;
+        int mid = s+(e-s)/2; 
+        while(s<e){
+            // you can also use s<=e but in that case if(arr[mid]>arr[mid+1])=> e = mid;
+            if(arr[mid]<arr[mid+1]){
+                s=mid+1;
+            }
+            else if(arr[mid]>arr[mid+1]){
+                e = mid;
+            }
+            mid = s+(e-s)/2;
+        }
+        return mid;
+    }
+}
