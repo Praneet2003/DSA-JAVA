c91a801dfdbdc648d5d42fab09a3cfd776bbc451
Praneet2003 <rajpraneet4@gmail.com>
2026-01-30 09:36:02 +0530

    Selection sort by selecting the maximum element and swaping it with element at index n-1 (unstable sorting algorithm)

--- Sorting Algorithms/Selection sort/Greatest_number_Selection_sort.java
+++ Sorting Algorithms/Selection sort/Greatest_number_Selection_sort.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Selection_sort{
+class Greatest_number_Selection_sort{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
