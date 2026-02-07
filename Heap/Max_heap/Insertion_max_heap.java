90d96015698340bd05f5a7c5c9800d6542bb19cd
Praneet2003 <rajpraneet4@gmail.com>
2026-02-07 09:15:24 +0530

    Insertion in max heap using the concept of upheap, which check all parent element till root so that the max heap property is followed

---
+++ Heap/Max_heap/Insertion_max_heap.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Insertion_max_heap {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Heap obj = new Heap();
+        for(int i=0;i<9;i++){
+            int n = sc.nextInt();
+            obj.insert(n);
+        }
+        obj.display();
+    }    
+}
+class Heap{
+    ArrayList<Integer> list = new ArrayList<>();
+    //insertion in heap takes time of O(log n)
+    public void insert(int val){
+        list.add(val);
+        upheap(list.size()-1);
+    }
+    private int parent(int i){
+        return (i-1)/2;
+    }
+    private void upheap(int i){
+        int p = parent(i);
+        if(list.get(p)<list.get(i)){
+            int temp = list.get(p);
+            list.set(p,list.get(i));
+            list.set(i,temp);
+            upheap(p);
+        }
+    }
+    public void display(){
+        for(int i = 0;i<list.size();i++){
+            System.out.print(list.get(i)+" ");
+        }
+    }
+}
+// Input: 9 9 11 11 2 3 4 2 1
+// Output: 11 11 9 9 2 3 4 2 1 (max element at the root acess time is O(1))
