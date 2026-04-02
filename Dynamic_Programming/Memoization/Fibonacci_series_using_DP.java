c49383aa0be43a972f90b01f1217d895dc8484d7
Praneet2003 <rajpraneet4@gmail.com>
2026-04-02 22:29:56 +0530

    fibonacci series using dp array for storing the result of sub problems

---
+++ Dynamic_Programming/Memoization/Fibonacci_series_using_DP.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Fibonacci_series_using_DP{
+    static int DP[];
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        DP= new int[n+1];
+        Arrays.fill(DP,-1);
+        System.out.println(n+" th Fibonacci number is: "+fibo(n));
+    }
+    public static int fibo(int n){
+        if(n<=1){
+            return n;
+        }
+        if(DP[n]!=-1){
+            return DP[n];
+        }
+        DP[n] = fibo(n-1)+fibo(n-2);
+        return DP[n];
+    }
+}
