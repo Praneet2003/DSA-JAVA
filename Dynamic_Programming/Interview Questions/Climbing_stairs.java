01f5e798c982a33800e5cf6f66aa5ede38eb50a9
Praneet Raj <rajpraneet4@gmail.com>
2026-05-31 23:29:05 +0530

    finding no of ways to reach at the top of stairs either you can jump 1 step or 2 steps at a time

--- Dynamic_Programming/Interview Questions/Climbing_stairs.java
+++ Dynamic_Programming/Interview Questions/Climbing_stairs.java
@@ -1,6 +1,6 @@
 // you are given n stairs and you have to reach at the top (i.e on nth stair).
 // either you can jump 1 stair or 2 stair.
-// you need to return the distinct no of ways in which you can reach at the top
+// you need to return the distinct no of ways in which you can reach at the top.
 import java.util.*;
 public class Climbing_stairs {
     public static void main(String[] args) {
@@ -18,7 +18,7 @@ class Solution{
         if(n<=1){
             return 1;
         }
-        if(dp[n]!=-1){
+        if(dp[n]!=-1)b{
             return dp[n];
         }
         dp[n] = getno_of_ways(n-1)+getno_of_ways(n-2);
